8545072a880dac9913c49d9e41d1d30c25eca062
vate <806019582@qq.com>
2022-03-27 22:35:56 +0800

    init

---
+++ readme.md
@@ -0,0 +1,5 @@
+### 使用手册
+在test目录中创建类来调试
+只需要设置idea中Jdk的源码路径为 src/source目录即可
+
+![img.png](img.png)
--- src/main/java/cn/vate/jdk/test/TestHashMap.java
+++ src/main/java/cn/vate/jdk/test/TestHashMap.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 public class TestHashMap {
 
     public static void main(String[] args) {
+        HashMap<String, String> stringStringHashMap = new HashMap<>();
     }
+
 
 }
